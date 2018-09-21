48cdbbabcf1fdf2f7ebde000cc87833b902b9de5
BlorZ <dupont.david21@hotmail.fr>
2018-09-21 14:02:02 +0200

    initialisation des classes Sac et Objet

---
+++ projet_opti/src/projet_opti/Objet.java
@@ -0,0 +1,26 @@
+package projet_opti;
+
+public class Objet {
+	private int poids;
+	private int valeur;
+	
+	public Objet() {
+		super();
+	}
+
+	public int getPoids() {
+		return poids;
+	}
+
+	public void setPoids(int poids) {
+		this.poids = poids;
+	}
+
+	public int getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(int valeur) {
+		this.valeur = valeur;
+	}
+}
---
+++ projet_opti/src/projet_opti/Sac.java
@@ -0,0 +1,29 @@
+package projet_opti;
+
+public class Sac {
+
+	private int valeur;
+	private int poidsMax;
+	
+	public Sac() {
+		super();
+	}
+
+	public int getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(int valeur) {
+		this.valeur = valeur;
+	}
+
+	public int getPoidsMax() {
+		return poidsMax;
+	}
+
+	public void setPoidsMax(int poidsMax) {
+		this.poidsMax = poidsMax;
+	}
+	
+	
+}
